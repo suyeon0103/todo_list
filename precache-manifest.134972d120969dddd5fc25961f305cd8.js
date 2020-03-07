17d60c0704f126d85f44494a7c44cb15b455b479
suyeon0103 <k_sy0103@naver.com>
2020-03-07 23:01:25 +0900

    Updates

---
+++ precache-manifest.134972d120969dddd5fc25961f305cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08eacfa0dd965001dd1207544aca72c6",
+    "url": "/todo_list/index.html"
+  },
+  {
+    "revision": "fa704969591191f05e77",
+    "url": "/todo_list/static/css/main.a00f981c.chunk.css"
+  },
+  {
+    "revision": "316aa01f96d159ee6685",
+    "url": "/todo_list/static/js/2.4f15f601.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/todo_list/static/js/2.4f15f601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa704969591191f05e77",
+    "url": "/todo_list/static/js/main.b13cb6c5.chunk.js"
+  },
+  {
+    "revision": "aac13f6b509f68ce0f63",
+    "url": "/todo_list/static/js/runtime-main.62b35e3d.js"
+  }
+]);
